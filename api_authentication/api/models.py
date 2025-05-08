ce981001f5f414c5a3759435e91b42fc8c6487f6
Mpho Mofokeng <mpmofokjhb024@studen.wethinkcode.co.za>
2025-05-08 15:50:28 +0200

    Added the user model for rthe tables

--- api_authentication/api/models.py
+++ api_authentication/api/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-      user_id = models.CharField( )
-      user_email = 
+      user_id = models.CharField(max_length=50)
+      user_email = models.CharField(max_length=50)
+    
+# So this basically creates the tables for the database   
+      def __str__(self):
+            return self.user_email
+      
+      def __str__(self):
+            return self.user_id
