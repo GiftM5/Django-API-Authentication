efc4b9427d0bbbfb0c835180a7de0d05c8a1098c
Mpho Mofokeng <mpmofokjhb024@studen.wethinkcode.co.za>
2025-05-09 17:39:44 +0200

    Added the new url patterns

--- api_authentication/api/urls.py
+++ api_authentication/api/urls.py
@@ -3,8 +3,8 @@ from .views import login,get_user_emails,create_user,user_details,signup,test_to
 
 urlpatterns = [    
     path('login/',login, name='User_Login'),
-    path('signup/',signup, name='User_Login'),
-    path('test_token/',test_token, name='User_Login'),
+    path('signup/',signup, name='User_signup'),
+    path('test_token/',test_token, name='test_token'),
     path('users/',get_user_emails,name = 'user_emails'),
     path('users/create/', create_user, name='create_user'),
     path('users/<int:pk>', user_details, name='user_details'),
