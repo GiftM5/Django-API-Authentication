95f4661673c1e43452f6394eb82af86673e1f56e
Mpho Mofokeng <mpmofokjhb024@studen.wethinkcode.co.za>
2025-05-09 15:19:56 +0200

    CHANGED THE VIEWS.PY

--- api_authentication/api/urls.py
+++ api_authentication/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_user_email, create_user
+from .views import get_user_email, create_user,user_details
 
 urlpatterns = [    
     path('users/', get_user_email, name='get_user_email'),
-    path('users/create/', create_user, name='create_user'), 
+    path('users/create/', create_user, name='create_user'),
+    path('users/<int:pk>', user_details, name='user_details'),
 ]
