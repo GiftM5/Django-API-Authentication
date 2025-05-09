365876fc3ddab856db373523aaf40c4736efaf98
Mpho Mofokeng <mpmofokjhb024@studen.wethinkcode.co.za>
2025-05-09 17:39:17 +0200

    Added the new url patterns

--- api_authentication/api/urls.py
+++ api_authentication/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import login,get_user_emails,create_user,user_details
+from .views import login,get_user_emails,create_user,user_details,signup,test_token
 
 urlpatterns = [    
     path('login/',login, name='User_Login'),
-    path('signup/',login, name='User_Login'),
-    path('test_token/',login, name='User_Login'),
+    path('signup/',signup, name='User_Login'),
+    path('test_token/',test_token, name='User_Login'),
     path('users/',get_user_emails,name = 'user_emails'),
     path('users/create/', create_user, name='create_user'),
     path('users/<int:pk>', user_details, name='user_details'),
